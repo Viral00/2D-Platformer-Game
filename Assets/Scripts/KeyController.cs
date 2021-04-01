4e14d34b0bb8661269d012d634953887ab58ed33
Viral00 <80312069+Viral00@users.noreply.github.com>
2021-04-02 04:12:02 +0530

    Collectible key and Score.

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collison)
+    {
+        if (collison.gameObject.GetComponent<PlayerController>())
+        {
+            PlayerController playerController = collison.gameObject.GetComponent<PlayerController>();
+            playerController.PickupKey();
+            Destroy(gameObject);
+        }
+    }
+}
